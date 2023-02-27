7ce9febd15c6d73436fbb0bc5436e0d64e7838e1
abdelqaouiNabil <abdelqaoui.nabil@gmail.com>
2023-02-27 09:52:45 +0100

    cc

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -19,35 +19,35 @@ class Dashboard extends Component
 
     public function render()
     {
-        // $caisse = 1;
-        // $depots = Depot::where('id_caisse', $caisse)->pluck('id');
-        // $retraits = Retrait::where('id_caisse', $caisse)->pluck('id');
-        $domaines = DB::select('select count(*) as domaineFois , d.name from fournisseurs f, f_domaines d
-        where f.id_fdomaine = d.id group by d.name');
+        // // $caisse = 1;
+        // // $depots = Depot::where('id_caisse', $caisse)->pluck('id');
+        // // $retraits = Retrait::where('id_caisse', $caisse)->pluck('id');
+        // $domaines = DB::select('select count(*) as domaineFois , d.name from fournisseurs f, f_domaines d
+        // where f.id_fdomaine = d.id group by d.name');
 
-        // dd($domaines);
-        // $mois = 1;
-        // $nbrDepots = DB::select("call nbrDepotParCaisse (".$mois.", ".$idCaisse.")");
-        $idCaisse = 1;
-        $ListeDepot = [];
-        for ($mois = 1; $mois <= 12; $mois++){
-            $nbrDepots = DB::select("call nbrDepotParCaisse (".$mois.", ".$idCaisse.")");
-            $nbrDepotsScalar = $nbrDepots[0]->nbrDepot;
-            $nbrDepotsString = strval($nbrDepotsScalar);
-            array_push($ListeDepot, (object) ['mois'=>$mois, 'nbrDep'=>$nbrDepotsString]);
-        }
-        // dd($ListeDepot);
-        $projets = Projet::all();
-        $fournisseurs = Fournisseur::all();
-        $caisse = Caisse::where('id', 1)->first();
+        // // dd($domaines);
+        // // $mois = 1;
+        // // $nbrDepots = DB::select("call nbrDepotParCaisse (".$mois.", ".$idCaisse.")");
+        // $idCaisse = 1;
+        // $ListeDepot = [];
+        // for ($mois = 1; $mois <= 12; $mois++){
+        //     $nbrDepots = DB::select("call nbrDepotParCaisse (".$mois.", ".$idCaisse.")");
+        //     $nbrDepotsScalar = $nbrDepots[0]->nbrDepot;
+        //     $nbrDepotsString = strval($nbrDepotsScalar);
+        //     array_push($ListeDepot, (object) ['mois'=>$mois, 'nbrDep'=>$nbrDepotsString]);
+        // }
+        // // dd($ListeDepot);
+        // $projets = Projet::all();
+        // $fournisseurs = Fournisseur::all();
+        // $caisse = Caisse::where('id', 1)->first();
 
-        // dd($domaines);
+        // // dd($domaines);
         return view('livewire.owner.dashboard', [
-            'domaines'=> $domaines,
-            'projets'=> $projets,
-            'fournisseurs'=>$fournisseurs,
-            'caisse'=> $caisse,
-            'ListeDepot'=>$ListeDepot
+            // 'domaines'=> $domaines,
+            // 'projets'=> $projets,
+            // 'fournisseurs'=>$fournisseurs,
+            // 'caisse'=> $caisse,
+            // 'ListeDepot'=>$ListeDepot
 
         ]);
     }
